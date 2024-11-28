9d3297861a31969fc84c32c08b21fca6f278f491
kerloshany3 <kerlostablet@gmail.com>
2024-11-29 00:34:11 +0200

    add design

--- app/components/Bannerl.jsx
+++ app/components/Bannerl.jsx
@@ -7,6 +7,8 @@ import Image from 'next/image'
 
 export const Bannerl = ({ courseslist }) => {
 
+
+    const raqamelsection = 10
     const [days, setdays] = useState([])
     useEffect(() => {
         datadays()
@@ -18,23 +20,25 @@ export const Bannerl = ({ courseslist }) => {
             setdays(resp.testos)
         })
     }
+    const sectionsfitlered = days.filter((item)=>(item.numberofsection === raqamelsection ))
     return (
         <div>
-
             <div className=' bg-white rounded-2xl w-4/5 m-auto'>
-
-                <h3 className=' text-black'></h3>
-
-                {days.map((item, index) => (
-                    <li key={index} className=' text-black font-rakkas text-5xl m-4 p-3'>{item.sectionname}</li>
+                <h3 className=' text-white font-rakkas text-5xl bg-red-500 m-auto flex justify-center p-3 border-4 border-white rounded-t-xl'>سكاشن يوم السبت</h3>
+                
+                {sectionsfitlered.map((item, index) => (
+                    item.numofweek === 1 && (
+                        <li key={index} className=' text-black font-rakkas text-5xl m-4 p-6 '>{item.sectionname}</li>
+                    )
                 ))}
-
             </div>
-
-
-
-
-
+            <div className=' bg-white rounded-2xl w-4/5 m-auto'>
+                <h3 className=' text-white font-rakkas text-5xl bg-red-500 m-auto flex justify-center p-3 border-4 border-white rounded-t-xl'>سكاشن يوم الاحد</h3>
+                {sectionsfitlered.map((item, index) => (
+                    item.numofweek === 2 && (
+                        <li key={index} className=' text-black font-rakkas text-5xl m-4 p-6 '>{item.sectionname}</li>
+                    )))}
+            </div>
         </div>
     )
 }
